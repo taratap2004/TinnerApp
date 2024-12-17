ff8d2e9de081c3b27ffd879865d2c5a771269480
taratap2004 <taralnw226059@gmail.com>
2024-12-17 10:14:33 +0700

    week6

--- server/src/configs/database.config.ts
+++ server/src/configs/database.config.ts
@@ -4,7 +4,7 @@ const username = Bun.env.MONGODBUSERNAME || 'your_username'
 const password = Bun.env.MONGODBPASSWORD || 'your_password'
 const db_name = Bun.env.MONGODB_DBNAME || 'TinnerApp'
 
-const uri = `mongodb+srv://${username}:${password}@taratap.a2wlw.mongodb.net/?retryWrites=true&w=majority&appName=${db_name}`
+const uri = `mongodb+srv://${username}:${password}  @taratap.a2wlw.mongodb.net/?retryWrites=true&w=majority&appName=${db_name}`
 
 export const MongoDB = {
     connect: async function () {
