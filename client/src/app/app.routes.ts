27f674b16ac3e210b41f9b6958d157323438e67d
taratap2004 <taralnw226059@gmail.com>
2024-12-25 10:37:24 +0700

    WEEK8

---
+++ client/src/app/app.routes.ts
@@ -0,0 +1,27 @@
+import { Routes } from '@angular/router'
+import { HomeComponent } from './home/home.component'
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./login/login.component').then(c => c.LoginComponent)
+    },
+    {
+        path: 'server-error',
+        loadComponent: () => import('./server-error/server-error.component').then(c => c.ServerErrorComponent)
+    },
+    {
+        path: '404',
+        loadComponent: () => import('./not-found/not-found.component').then(c => c.NotFoundComponent)
+    },
+    {
+        path: '**',
+        pathMatch: 'full',
+        loadComponent: () => import('./not-found/not-found.component').then(c => c.NotFoundComponent)
+    },
+
+]
